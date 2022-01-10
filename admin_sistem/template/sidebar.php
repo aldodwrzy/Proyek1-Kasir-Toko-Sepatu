d25b260cbe1a4c2d0b6853a962fc1314de8cbedf
mraihanna <mraihanna1278@gmail.com>
2022-01-10 09:09:42 +0700

    Iseng doang ini mah

--- admin_sistem/template/sidebar.php
+++ admin_sistem/template/sidebar.php
@@ -1,87 +1,85 @@
-   
-      <!-- **********************************************************************************************************************************************************
-      MAIN SIDEBAR MENU
-      *********************************************************************************************************************************************************** -->
-      <!--sidebar start-->
-<?php 
-  $id = $_SESSION['admin sistem']['id_member'];
-  $hasil_profil = $lihat -> member_edit($id);
+<!-- **********************************************************************************************************************************************************
+    MAIN SIDEBAR MENU
+    *********************************************************************************************************************************************************** -->
+<!--sidebar start-->
+<?php
+$id = $_SESSION['admin sistem']['id_member'];
+$hasil_profil = $lihat->member_edit($id);
 ?>
-      <aside>
-          <div id="sidebar"  class="nav-collapse ">
-              <!-- sidebar menu start-->
-              <ul class="sidebar-menu" id="nav-accordion">
-              
-              	  <p class="centered"><a><img src="assets/img/user/<?php echo $hasil_profil['gambar'];?>" class="img-circle" width="100" height="110"></a></p>
-              	  <h5 class="centered"><?php echo $hasil_profil['nm_member'];?></h5>
-              	  <h5 class="centered">( <?php echo $hasil_profil['NIK'];?> )</h5>
-              	  <h5 class="centered">Pemilik (Admin Sistem)</h5>
-              	  	
-                  <li class="mt">
-                      <a href="index.php">
-                          <i class="fa fa-dashboard"></i>
-                          <span>Dashboard</span>
-                      </a>
-                  </li>
+<aside>
+    <div id="sidebar" class="nav-collapse ">
+        <!-- sidebar menu start-->
+        <ul class="sidebar-menu" id="nav-accordion">
 
-                  <li class="sub-menu">
-                      <a href="index.php?page=supplier">
-                          <i class="fa fa-desktop"></i>
-                          <span>Supplier</span>
-                      </a>
-                  </li>
+            <p class="centered"><a><img src="assets/img/user/<?php echo $hasil_profil['gambar']; ?>" class="img-circle" width="100" height="110"></a></p>
+            <h5 class="centered"><?php echo $hasil_profil['nm_member']; ?></h5>
+            <h5 class="centered">( <?php echo $hasil_profil['NIK']; ?> )</h5>
+            <h5 class="centered">Pemilik (Admin Sistem)</h5>
 
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>Sepatu <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="index.php?page=kategori">Kategori</a></li>
-                          <li><a  href="index.php?page=barang">Sepatu</a></li>
-                          <li><a  href="index.php?page=ukuran">Ukuran</a></li>
-                      </ul>
-                  </li>
+            <li class="mt">
+                <a href="index.php">
+                    <i class="fa fa-dashboard"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
 
+            <li class="sub-menu">
+                <a href="index.php?page=supplier">
+                    <i class="fa fa-desktop"></i>
+                    <span>Supplier</span>
+                </a>
+            </li>
 
+            <li class="sub-menu">
+                <a href="">
+                    <i class="fa fa-desktop"></i>
+                    <span>Sepatu <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li><a href="index.php?page=kategori">Kategori</a></li>
+                    <li><a href="index.php?page=barang">Sepatu</a></li>
+                    <li><a href="index.php?page=ukuran">Ukuran</a></li>
+                </ul>
+            </li>
 
 
 
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>Transaksi <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="index.php?page=jual">Transaksi Jual</a></li>
-                          <li><a  href="index.php?page=beli">Transaksi Beli</a></li>
-                      </ul>
-                  </li>
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-desktop"></i>
-                          <span>Laporan <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a  href="index.php?page=laporan">Laporan Penjualan</a></li>
-                          <li><a  href="index.php?page=laporanbeli">Laporan Pembelian</a></li>
-                          <li><a  href="index.php?page=laporanstok">Laporan Stok</a></li>
-                      </ul>
-                  </li>
-                 
-                  <li class="sub-menu">
-                      <a href="javascript:;" >
-                          <i class="fa fa-cog"></i>
-                          <span>Setting <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
-                      </a>
-                      <ul class="sub">
-                          <li><a href="index.php?page=pengaturan">Pengaturan Toko</a></li>
-                          <li><a  href="index.php?page=user">User</a></li>
-                      </ul>
-                  </li>
-              </ul>
-              <!-- sidebar menu end-->
-          </div>
-      </aside>
-      <!--sidebar end-->
-      
+
+
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-desktop"></i>
+                    <span>Transaksi <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li><a href="index.php?page=jual">Transaksi Jual</a></li>
+                    <li><a href="index.php?page=beli">Transaksi Beli</a></li>
+                </ul>
+            </li>
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-desktop"></i>
+                    <span>Laporan <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li><a href="index.php?page=laporan">Laporan Penjualan</a></li>
+                    <li><a href="index.php?page=laporanbeli">Laporan Pembelian</a></li>
+                    <li><a href="index.php?page=laporanstok">Laporan Stok</a></li>
+                </ul>
+            </li>
+
+            <li class="sub-menu">
+                <a href="javascript:;">
+                    <i class="fa fa-cog"></i>
+                    <span>Setting <span style="padding-left:2px;"> <i class="fa fa-angle-down"></i></span></span>
+                </a>
+                <ul class="sub">
+                    <li><a href="index.php?page=pengaturan">Pengaturan Toko</a></li>
+                    <li><a href="index.php?page=user">User</a></li>
+                </ul>
+            </li>
+        </ul>
+        <!-- sidebar menu end-->
+    </div>
+</aside>
+<!--sidebar end-->
